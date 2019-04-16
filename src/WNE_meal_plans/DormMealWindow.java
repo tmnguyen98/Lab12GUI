f485e96d8bd719be8de669e7a3112bb12aeb7eec
H103 <manhtuan88394@gmail.com>
2019-04-16 15:27:15 -0400

    Show 2 drop boxes

--- src/WNE_meal_plans/DormMealWindow.java
+++ src/WNE_meal_plans/DormMealWindow.java
@@ -1,17 +1,25 @@
 package WNE_meal_plans;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DormMealWindow extends JFrame {
 
-    private JComboBox dropdown;
+    private JComboBox Dormdropdown;
+    private JComboBox Mealdropdown;
+    private JPanel panel1;
+    private JPanel panel2;
     private JLabel label;
+    private final int WINDOW_WIDTH = 400;	// Window width
+    private final int WINDOW_HEIGHT = 100;	// Window height
 
     public DormMealWindow() {
         // Set the title bar text
         setTitle("Order Calculator");
+
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
         // Specify an action for the close button
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,7 +28,6 @@ public class DormMealWindow extends JFrame {
         buildPanel();
 
         //Display the window
-        pack();
         setVisible(true);
 
     }
@@ -28,10 +35,18 @@ public class DormMealWindow extends JFrame {
     private void buildPanel() {
         // Create dropdown and text stuff
         String[] dorms = {"Plymouth", "Commonwealth", "Gateway", "Southwood"};
-        dropdown = new JComboBox(dorms);
+        String[] mealplan = {"7 meals per week ", "14 meals per week", "Unlimited meal"};
+        Dormdropdown = new JComboBox(dorms);
+        Mealdropdown = new JComboBox(mealplan);
         //dropdown.addActionListener(new DropdownListener());
-        add(dropdown);
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+        panel1.add(Dormdropdown);
+        panel2.add(Mealdropdown);
         //add(label);
+
+        add(panel1, BorderLayout.EAST);
+        add(panel2, BorderLayout.WEST);
     }
 
     public static void main(String[] args) {
@@ -42,7 +57,13 @@ public class DormMealWindow extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            if (e.getSource() == Dormdropdown) {
+                JComboBox item = (JComboBox) e.getSource();
+                String message = (String) item.getSelectedItem();
+                switch (message) {
+                    case "Pymouth": label.setText("");
+                }
+            }
         }
     }
 }
