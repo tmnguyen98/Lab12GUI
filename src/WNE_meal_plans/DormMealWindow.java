b7209586beceab99274e0711f9b05caa62a8433c
H103 <manhtuan88394@gmail.com>
2019-04-16 15:00:46 -0400

    Add dropdown menu

--- src/WNE_meal_plans/DormMealWindow.java
+++ src/WNE_meal_plans/DormMealWindow.java
@@ -1,8 +1,13 @@
 package WNE_meal_plans;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class DormMealWindow extends JFrame{
+public class DormMealWindow extends JFrame {
+
+    private JComboBox dropdown;
+    private JLabel label;
 
     public DormMealWindow() {
         // Set the title bar text
@@ -11,13 +16,33 @@ public class DormMealWindow extends JFrame{
         // Specify an action for the close button
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //Build the panel and add it to the frame
+        buildPanel();
+
         //Display the window
         pack();
         setVisible(true);
 
     }
 
+    private void buildPanel() {
+        // Create dropdown and text stuff
+        String[] dorms = {"Plymouth", "Commonwealth", "Gateway", "Southwood"};
+        dropdown = new JComboBox(dorms);
+        //dropdown.addActionListener(new DropdownListener());
+        add(dropdown);
+        //add(label);
+    }
+
     public static void main(String[] args) {
         new DormMealWindow();
+    }
+
+    private class DropdownListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
